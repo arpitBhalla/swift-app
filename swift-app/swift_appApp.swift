6e8ca3d672fcaa8229e7e2c9047218984645ec14
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-12-19 14:05:11 +0530

    Initial Commit

---
+++ swift-app/swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_appApp.swift
+//  swift-app
+//
+//  Created by Arpit Bhalla on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
